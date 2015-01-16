08a2d1f37a149e42379535d805891d55de5f8c69
Sönke Lange <soenkelange@aol.com>
2015-01-16 18:18:57 +0100

    Add abstract methods handleOpenCamera and handleOpenFile to AbstractProjectWidget

--- app/abstractprojectwidget.h
+++ app/abstractprojectwidget.h
@@ -17,6 +17,23 @@ public:
 
     AbstractProjectInfo* projectInfo() const;
 
+    /**
+     * Opens the camera which was selected in MainWindow
+     *
+     * @brief handleOpenCamera
+     * @param device to open
+     * @return bool true, if the device was opened, or false
+     */
+    virtual bool handleOpenCamera(int device) = 0;
+
+    /**
+     * Opens the file which was selected in MainWindow
+     *
+     * @brief handleOpenFile
+     * @param file
+     */
+    virtual void handleOpenFile(QString file) = 0;
+
 private:
     AbstractProjectInfo *_projectInfo;
     QLayout *layout;
