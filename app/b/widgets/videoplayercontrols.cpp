e151704a27ec3905b590e3350ff9f5114882a820
Sönke Lange <soenkelange@aol.com>
2015-01-20 19:22:33 +0100

    Add functionality to VideoPlayerControls
    - Clicking play or stop will trigger signals
    - Possibility to connect to the signals of the VideoPlayer to toggle buttons

--- app/b/widgets/videoplayercontrols.cpp
+++ app/b/widgets/videoplayercontrols.cpp
@@ -1,16 +1,53 @@
 #include "videoplayercontrols.h"
 #include "ui_videoplayercontrols.h"
 
+#include <QDebug>
+
 VideoPlayerControls::VideoPlayerControls(QWidget *parent) :
     QGroupBox(parent),
     ui(new Ui::VideoPlayerControls)
 {
     ui->setupUi(this);
     ui->playButton->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
+    ui->playButton->setEnabled(true);
+    connect(ui->playButton, SIGNAL(clicked()), this, SLOT(playClicked()));
+
     ui->stopButton->setIcon(style()->standardIcon(QStyle::SP_MediaStop));
+    ui->stopButton->setEnabled(false);
+    connect(ui->stopButton, SIGNAL(clicked()), this, SLOT(stopClicked()));
 }
 
 VideoPlayerControls::~VideoPlayerControls()
 {
     delete ui;
+}
+
+void VideoPlayerControls::setState(VideoPlayer::State state)
+{
+    qDebug() << "VideoPlayerControls::setState" << state;
+    if (state != playerState) {
+        switch(state) {
+        case VideoPlayer::Stopped:
+            ui->stopButton->setEnabled(false);
+            ui->playButton->setEnabled(true);
+            break;
+        case VideoPlayer::Playing:
+            ui->stopButton->setEnabled(true);
+            ui->playButton->setEnabled(false);
+            break;
+        }
+        playerState = state;
+    }
+}
+
+void VideoPlayerControls::playClicked()
+{
+    qDebug() << "VideoPlayerControls::playClicked";
+    emit play();
+}
+
+void VideoPlayerControls::stopClicked()
+{
+    qDebug() << "VideoPlayerControls::stopClicked";
+    emit stop();
 }
--- app/b/widgets/videoplayercontrols.h
+++ app/b/widgets/videoplayercontrols.h
@@ -1,6 +1,7 @@
 #ifndef VIDEOPLAYERCONTROLS_H
 #define VIDEOPLAYERCONTROLS_H
 
+#include "../videoengine/videoplayer.h"
 #include <QGroupBox>
 
 namespace Ui {
@@ -15,8 +16,20 @@ public:
     explicit VideoPlayerControls(QWidget *parent = 0);
     ~VideoPlayerControls();
 
+public slots:
+    void setState(VideoPlayer::State state);
+
+signals:
+    void play();
+    void stop();
+
+private slots:
+    void playClicked();
+    void stopClicked();
+
 private:
     Ui::VideoPlayerControls *ui;
+    VideoPlayer::State playerState;
 };
 
 #endif // VIDEOPLAYERCONTROLS_H
